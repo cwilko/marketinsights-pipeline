e4538f3520111ab94a1c777ed248b9d3dccea2a7
cwilko <chris.wilkinson@uk.ibm.com>
2018-07-31 23:07:59 +0100

    Integrate with pytest

--- tests/unittests/test_Pipelines.py
+++ tests/unittests/test_Pipelines.py
@@ -1,8 +1,11 @@
 import unittest
 import pandas
 import json
+import os
 from quantutils.api.auth import CredentialsStore
 from quantutils.api.marketinsights import MarketInsights, Dataset
+
+root_dir = os.path.dirname(os.path.abspath(__file__)) + "/"
 
 class PipelineTest(unittest.TestCase):
 
@@ -11,9 +14,9 @@ class PipelineTest(unittest.TestCase):
 
     def testDatasetGeneration(self):
 
-        #DATASET_ID = "265e2f7f3e06af1c6fc9e74434514c86"
         DATASET_ID="4234f0f1b6fcc17f6458696a6cdf5101"
-        dataset_desc_file = "../../../marketinsights-data/datasets/WallSt-FinalTradingHour.json"
+
+        dataset_desc_file = root_dir + "../../../marketinsights-data/datasets/WallSt-FinalTradingHour.json"
         with open(dataset_desc_file) as data_file:    
             dataset_desc = json.load(data_file)["dataset_desc"]
 
@@ -22,7 +25,7 @@ class PipelineTest(unittest.TestCase):
 
         ds,_ = self.mi.get_dataset_by_id(DATASET_ID)
 
-        expectedDS = pandas.read_csv("testDataset_DOW.csv", index_col=0, parse_dates=True, float_precision='round_trip')
+        expectedDS = pandas.read_csv(root_dir + "testDataset_DOW.csv", index_col=0, parse_dates=True, float_precision='round_trip')
         expectedDS.index = expectedDS.index.tz_localize("UTC").tz_convert("US/Eastern")
         expectedDS.columns = ds.columns
 
